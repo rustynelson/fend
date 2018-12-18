682f77ce10d8a2835fb95cdbbd9eb0e5fc2bb9c2
ed <ed.wodka@motorolasolutions.com>
2018-12-18 08:54:35 -0600

    initial cat clicker hacks to get it to work fairly well

---
+++ images/burly_files/r-sf_data_002/78704.js
@@ -0,0 +1,48 @@
+BAP.copyJSON({
+  "data": {
+    "icon_position": "top-right",
+    "default_icon": "_us",
+    "mobile_in_app_url": "",
+    "default_footer": "Privacy Controls by Evidon, Inc.",
+    "icon_display": "normal",
+    "icon_grayscale": 30,
+    "container_opacity": 70,
+    "offset_x": 0,
+    "offset_y": 0,
+    "generic_icon": false,
+    "icon_delay": 0,
+    "nid": 78704,
+    "nwid": null,
+    "aid": 680,
+    "icid": null,
+    "skip_L2": false,
+    "behavioral": "custom",
+    "generic_text": "This ad has been matched to your interests.  It was selected for you based on your browsing activity.  Target used a third party ad serving and targeting platform to determine that you might be interested in an ad like this.",
+    "adv_name": "Target",
+    "adv_msg": "Target’s Privacy Policy",
+    "adv_logo": "https://c.betrad.com/a/l/n/680/57991/us/19667126-969e-43d1-9d8b-273bba03d90d.png",
+    "adv_link": "http://www.target.com/spot/privacy-policy#?lnk=fnav_t_spc_2_3&intc=28073",
+    "mobile_message": "Tap to edit advertising preferences",
+    "display_mobile_overlay": true,
+    "mobile_advertiser_logo_url": "",
+    "default_icon_text": "AdChoices",
+    "default_generic1": "This ad has been matched to your interests. It was selected for you based on your browsing activity.",
+    "default_generic2": "This ad may have been matched to your interests based on your browsing activity.",
+    "default_generic3": "helped",
+    "default_generic4": "determine that you might be interested in an ad like this.",
+    "default_generic5": "select this ad for you.",
+    "default_generic6": "selected this ad for you.",
+    "default_link1": "More information & opt-out options",
+    "default_link2": "What is interest based advertising",
+    "default_link3": "Learn about your choices",
+    "ecid": null,
+    "hide_wi": false,
+    "hide_cl": false,
+    "server": [
+      {
+        "name": "DoubleClick"
+      }
+    ],
+    "message_properties": {}
+  }
+});
